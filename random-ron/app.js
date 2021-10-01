22b358209390807c0d2462ac08e1d6c1eaad2913
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2021-10-01 15:33:42 +0100

    Remove comment bloat

--- random-ron/app.js
+++ random-ron/app.js
@@ -1,53 +1,24 @@
 'use strict';
 
-//
-// Variables
-//
-
-// Save the API endpoint
 const api = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
 
 const quote = document.querySelector('#quote');
 const button = document.querySelector('#more-ron');
 
-
-//
-// Functions
-//
-
-/**
- * Get the JSON data from a Fetch request
- * @param {Response} response The Response object
- * @returns {Promise<any>} The JSON data or an Error object
- */
 function getJSON(response) {
-  // If the response was OK, return the JSON data
   if (response.ok) return response.json();
-
-  // Otherwise, return an Error object
   const error = new Error('Try again later.');
   return Promise.reject(error)
 }
 
-/**
- * Insert the quote into the DOM
- * @param {string[]} quotes The array of quotes
- */
 function insertQuote(quotes) {
   quote.textContent = quotes[0];
 }
 
-/**
- * Insert the error message into the DOM
- * @param {Error} error The Error object
- */
 function insertError(error) {
   quote.textContent = error.toString();
 }
 
-/**
- * Fetch a quote and insert it into the DOM
- */
 function getQuote() {
   fetch(api)
     .then(getJSON)
@@ -55,13 +26,6 @@ function getQuote() {
     .catch(insertError);
 }
 
-
-//
-// Inits & Event Listeners
-//
-
-// Show a quote
 getQuote();
 
-// Handle click events
 button.addEventListener('click', getQuote);
